37a839a87fc269ed1c1461043b2b6bbf17b29e00
anson inomwan <inomwanosaruese@gmail.com>
2026-01-04 18:45:20 -0500

    made changes to branch

---
+++ v-net3511/main.tf
@@ -0,0 +1,33 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# resource "azurerm_network_security_group" "nsg" {
+#   name                = "cloud-nsg"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "testing-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name             = "subnet1"
+    address_prefixes = ["10.0.1.0/24"]
+  }
+
+  subnet {
+    name             = "subnet2"
+    address_prefixes = ["10.0.2.0/24"]
+    #  security_group   = azurerm_network_security_group.nsg.id
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
---
+++ v-net3511/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "testing-vnet-rg"
+}
+
+variable "location" {
+  description = "Location of the resource group"
+  type        = string
+  default     = "canadacentral"
+}
